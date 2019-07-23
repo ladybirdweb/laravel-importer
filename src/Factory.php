ce17b52855b89c7ce6ef9dd81c807ebf6f948c55
Phaniraj35 <phaniraj.k@ladybirdweb.com>
2019-07-23 03:15:28 +0000

    Apply fixes from StyleCI

--- src/Factory.php
+++ src/Factory.php
@@ -2,22 +2,20 @@
 
 namespace LWS\Import;
 
-class Factory 
+class Factory
 {
     public function __construct()
     {
         $this->namespace = config('import.namespace');
     }
 
-
     public function make($source)
     {
         $source = ucfirst(strtolower($source));
-        $name = $this->namespace."\\".$source;
+        $name = $this->namespace.'\\'.$source;
 
-        if(class_exists($name)) {
+        if (class_exists($name)) {
             return new $name();
         }
-
     }
 }
